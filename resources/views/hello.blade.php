e9d24a8748eb409f1a773bcde002bab2ccef2ed4
ahmaddzidnii <zdngaming312@gmail.com>
2024-04-15 16:23:58 +0700

    2: Belajar Routing Sederhana dan View

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sapa Nama</title>
+</head>
+<body>
+    <h1 style="font-size: larger;">
+        Hello {{ $name }}
+    </h1>
+</body>
+</html>
